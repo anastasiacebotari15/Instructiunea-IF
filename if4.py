425e5ba4df7fa0bc9a2c26a20a5011b89b76ed18
Cebotari Anastasia <71016019+anastasiacebotari15@users.noreply.github.com>
2020-10-13 16:46:50 +0300

    Add files via upload

---
+++ if4.py
@@ -0,0 +1,10 @@
+#Se introduc vârstele a 3 persoane. Afişaţi vârstele cuprinse între 18 şi 60 de ani. 
+v1=int(input("v1="))
+v2=int(input("v2="))
+v3=int(input("v3="))
+if ((v1>18) and (v1<60)):
+    print(v1)
+if((v2>18) and (v2<60)):
+    print(v2)
+if((v3>18) and (v3<60)):
+    print(v3)
