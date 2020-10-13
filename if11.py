4256fcd17368f9cdd931ac495a65796a815dd489
Cebotari Anastasia <71016019+anastasiacebotari15@users.noreply.github.com>
2020-10-13 21:46:28 +0300

    Add files via upload

---
+++ if11.py
@@ -0,0 +1,13 @@
+"""Se dau trei numere. Să se afişeze aceste numere unul sub altul, afişând în dreptul fiecăruia unul dintre 
+cuvintele PAR sau IMPAR."""
+n1=int(Input("n1="))
+n2=int(input("n2="))
+n3=int(input("n3="))
+if(n1//2=0) or (n2//2=0) or (n3//2=0):
+    print(n1, "PAR" , sep="   ")
+    print(n2, "PAR" , sep="   ")
+    print(n3, "PAR" , sep="   ")
+else:
+    print(n1, "IMPAR" , sep="   ")
+    print(n2, "IMPAR" , sep="   ")
+    print(n3, "IMPAR" , sep="   ")
