f44bcf454cd33b08c0ffff02df40cea7ac0dfe8a
Cebotari Anastasia <71016019+anastasiacebotari15@users.noreply.github.com>
2020-10-14 20:28:16 +0300

    Add files via upload

---
+++ if3.py
@@ -0,0 +1,6 @@
+#Să se verifice dacă o literă introdusă este vocală sau consoană.
+litera=input("Litera:")
+if litera in(" a , e , i , u , o , ă , î "):
+    print("Vocala")
+else:
+    print("Consoana ")
